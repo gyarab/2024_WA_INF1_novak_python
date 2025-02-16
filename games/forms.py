0887db83b55cf279609e32d9a5d130b19e4fb02d
Ilja Novak <ilya496>
2025-02-16 07:18:06 +0100

    added games fixtures and change its model a bit

--- games/forms.py
+++ games/forms.py
@@ -4,12 +4,12 @@ from .models import Game
 class GameForm(forms.ModelForm):
     class Meta:
         model = Game
-        fields = ['name', 'description', 'price', 'download_count', 'categories', 'image']
+        fields = ['name', 'description', 'price', 'categories', 'platforms', 'image']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
             'price': forms.NumberInput(attrs={'class': 'form-control'}),
-            'download_count': forms.NumberInput(attrs={'class': 'form-control'}),
             'categories': forms.SelectMultiple(attrs={'class': 'form-select'}),
+            'platforms': forms.SelectMultiple(attrs={'class': 'form-select'}),
             'image': forms.FileInput(attrs={'class': 'form-control'}),
         }
--- games/models.py
+++ games/models.py
@@ -5,6 +5,15 @@ from django.utils.text import slugify
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+    
+class Platform(models.Model):
+    name = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
 
@@ -12,12 +21,12 @@ class Game(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    download_count = models.IntegerField(default=0)
     publisher = models.ForeignKey(User, on_delete=models.CASCADE)
     categories = models.ManyToManyField(Category)
     release_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='game_images/', null=True, blank=True)
     slug = models.SlugField(unique=True, blank=True)
+    platforms = models.ManyToManyField(Platform)
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -26,3 +35,11 @@ class Game(models.Model):
 
     def __str__(self):
         return self.name
+    
+    @property
+    def average_rating(self):
+        reviews = self.reviews.all()
+        if reviews.exists():
+            return reviews.aggregate(models.Avg('rating'))['rating__avg']
+        return None
+
