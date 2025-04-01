333a2d461e24b37a9aca4e606787fbc9ae3a30b2
Ilja Novak <birukov0608@gmail.com>
2025-04-01 22:08:09 +0200

    experimental page

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, experimental
 
 urlpatterns = [
   path('', home, name='home'),
+  path('experimental/', experimental, name='experimental'),
 ]
